cfd0b277b8b1c5478986f63cabde3b53fca0a73f
federico silva <fede.silva@gmail.com>
2019-05-30 21:44:52 -0300

    defined a schema

--- src/main/scala/example/ParquetExploreMain.scala
+++ src/main/scala/example/ParquetExploreMain.scala
@@ -1,9 +1,34 @@
 package example
 
+import org.apache.parquet.schema.{GroupType, MessageType, PrimitiveType}
+import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName.{BINARY, INT64}
+import org.apache.parquet.schema.Type.Repetition.{OPTIONAL, REPEATED, REQUIRED}
+
 // Julien's example
 //https://github.com/apache/parquet-mr/blob/apache-parquet-1.8.1/parquet-column/src/main/java/org/apache/parquet/example/Paper.java
 //
 
-object ParquetExploreMain extends  App {
+object ParquetExploreMain extends App {
   println("Blah")
+}
+
+object Schemas {
+  
+  val docSchema =
+    new MessageType("Document",
+      new PrimitiveType(REQUIRED, INT64, "DocId"),
+      new GroupType(OPTIONAL, "Links",
+        new PrimitiveType(REPEATED, INT64, "Backward"),
+        new PrimitiveType(REPEATED, INT64, "Forward")
+      ),
+      new GroupType(REPEATED, "Name",
+        new GroupType(REPEATED, "Language",
+          new PrimitiveType(REQUIRED, BINARY, "Code"),
+          new PrimitiveType(OPTIONAL, BINARY, "Country")
+        ),
+        new PrimitiveType(OPTIONAL, BINARY, "Url")
+      )
+    )
+  
+  
 }
